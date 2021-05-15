9b0e74aaa23524a034867f060d0bbe9bf1653601
Viktor Rezvantsev <geroid1735@gmail.com>
2021-05-15 17:08:44 +0300

    Add cocos2d project, begin add menu scene

---
+++ BattleGame/MenuScene.cpp
@@ -0,0 +1,8 @@
+//
+//  MenuScene.cpp
+//  BattleGame
+//
+//  Created by Viktor Rezvantsev on 15.05.2021.
+//
+
+#include "MenuScene.hpp"
---
+++ BattleGame/MenuScene.hpp
@@ -0,0 +1,13 @@
+//
+//  MenuScene.hpp
+//  BattleGame
+//
+//  Created by Viktor Rezvantsev on 15.05.2021.
+//
+
+#ifndef MenuScene_hpp
+#define MenuScene_hpp
+
+#include <stdio.h>
+
+#endif /* MenuScene_hpp */
